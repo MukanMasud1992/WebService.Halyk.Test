d2957fa2f22193e9ba18ad928a948342d51e7ca0
Massud <101732453+MukanMasud1992@users.noreply.github.com>
2023-02-24 00:17:39 +0600

    Update CREATE TABLE R_CURRENCY.sql

--- CREATE TABLE R_CURRENCY.sql
+++ CREATE TABLE R_CURRENCY.sql
@@ -1,10 +1,10 @@
 USE TEST
 CREATE TABLE R_CURRENCY
 (
-    Id INT PRIMARY KEY IDENTITY,
-    TITLE nvarchar(60),
-	CODE varchar(3),      
-    VALUE Decimal (18,2),       
-	A_DATE DATE
+   Id INT PRIMARY KEY IDENTITY,
+    TITLE nvarchar(60) not null,
+	CODE varchar(3)  not null,      
+    VALUE Decimal (18,2)  not null,       
+	A_DATE DATE  not null
 )
 DROP TABLE R_CURRENCY
